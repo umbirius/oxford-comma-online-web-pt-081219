f23eaa993c75622ad155bd8773b88e5d2ca3158c
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-16 03:12:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-array.join(", ")
+  i=0
+string =""
+while i<(array.length-2)
+  string << "#{array[i]}, "
+  i+=1
+end 
+string << ", and #{array.last}"
 end
+
+oxford_comma(["dog", "cat", "bird"])
