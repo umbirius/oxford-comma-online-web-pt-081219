f929e35114ee287544c0d814d40ea5b378d3a875
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-16 03:18:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,15 @@
 def oxford_comma(array)
   i=0
 string =""
-while i<(array.length-1)
-  string << "#{array[i]}, "
-  i+=1
+if array.length >1 
+  while i<(array.length-1)
+    string << "#{array[i]}, "
+    i+=1
+  end 
+  string << "and #{array.last}"
+else array.length <2  
+  string = array[i]
 end 
-string << "and #{array.last}"
 print string
 end
 
