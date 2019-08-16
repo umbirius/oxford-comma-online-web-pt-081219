466a066d3bb55cfdb354b06af3d46d3fe1b84048
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-16 03:22:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  i=0
+string =""
+if array.length >2 
+  while i<(array.length-1)
+    string << "#{array[i]}, "
+    i+=1
+  end 
+  string << "and #{array.last}"
+elsif array.length <2  
+  string = array[i]
+elsif array.length <3 
+  string = "#{array[i]} and #{array[1]}"
+end 
+p string
 end
+
+oxford_comma(["dog", "burd"])
